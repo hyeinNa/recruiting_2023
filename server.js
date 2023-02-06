e1de62dd6036daf2b26dcf62f9b03b7b8b86f2e1
yoongang02 <yoongang02@gmail.com>
2023-02-06 15:22:38 +0900

    #1 nodemon 설치 및 'backend' scripts 추가

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const mongoose = require("mongoose");
 const app = express();
 //.env에서 PORT와 MONGO_URI 가져오기
 const { PORT, MONGO_URI } = process.env;
+//body-parser 가져오기
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //mongoose를 이용하여 app과 몽고DB를 연결 함.
 mongoose.set("strictQuery", false);
