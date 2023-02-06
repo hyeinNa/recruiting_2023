b0150d0f91bac959a658d11c5ab3e26a56a7d284
yoongang02 <yoongang02@gmail.com>
2023-02-06 16:00:36 +0900

    #1 Proxy Server 구축 및 'dev' scripts 추가

---
+++ client/src/setupProxy.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+//Cors 이슈 해결을 위해, Proxy Server 구축
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: `http://localhost:3001`,
+      changeOrigin: true,
+    })
+  );
+};
