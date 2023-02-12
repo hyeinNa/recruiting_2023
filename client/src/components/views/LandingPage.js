f4d552cdc19c84d53fe4df4a469613c97e1da5ca
yoongang02 <yoongang02@gmail.com>
2023-02-13 06:40:14 +0900

    #6 #7 Footer Header 고정 및 Wrapper css 설정
    Footer와 Header가 내용 가리지 않도록 하는 Wrapper의 css 설정

--- client/src/components/views/LandingPage.js
+++ client/src/components/views/LandingPage.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
-
+import "./LandingPage.css";
 function LandingPage() {
   return (
     <div>
       <Header />
-
+      <div id="wrapper">
+        <h1>dd1</h1>
+        <h1>ddd</h1>
+        <h1>ddd</h1>
+        <h1>ddd</h1>
+        <h1>ddd</h1>
+        <h1>ddd</h1>
+        <h1>ddd</h1>
+        <h1>ddd</h1>
+        <h1>ddd</h1>
+        <h1>ddd</h1>
+        <h1>ddd</h1>
+        <h1>ddd</h1>
+      </div>
       <Footer />
     </div>
   );
