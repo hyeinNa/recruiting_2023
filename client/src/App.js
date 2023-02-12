28bab5589a06d3086fdb56d9a782272e0f05e7f7
MK-cyb <mindy50@ewhain.net>
2023-02-13 06:56:40 +0900

    main.js 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/views/LandingPage";
+import LandingPage from "./components/views/main/main";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/main" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </div>
