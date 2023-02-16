ee996e4b170a7b93b9d8a2fd3fff54d1df373d6a
yoongang02 <yoongang02@gmail.com>
2023-02-16 23:56:16 +0900

    #13 수정하기 페이지에 기존 지원자 정보 띄우기

--- client/src/components/views/UpdatePages/UpdatePage.js
+++ client/src/components/views/UpdatePages/UpdatePage.js
@@ -1,7 +1,76 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function UpdatePage() {
-  return <div>UpdatePage</div>;
+  //url에서 지원자 고유 id 알아내기
+  const pathName = window.location.pathname;
+  const applicantId = pathName.substring(8, pathName.length);
+  //입력 폼에 대한 변수와 함수
+  const [inputs, setInputs] = useState({
+    name: "",
+    studentId: "",
+    ewhaianId: "",
+  });
+  const { name, studentId, ewhaianId } = inputs;
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  //첫 렌더링 때, url에서 알아낸 지원자 id로 지원자 정보 가져와서 입력 폼에 반영하기
+  useEffect(() => {
+    axios
+      .post("/api/update/get", {
+        id: applicantId,
+      })
+      .then((response) => {
+        setInputs({
+          name: response.data.name,
+          studentId: response.data.studentId,
+          ewhaianId: response.data.ewhaianId,
+        });
+      })
+      .catch((error) => {
+        console.log("An error occurred: ", error.response);
+      });
+  }, []);
+
+  return (
+    <div>
+      UpdatePage 개인 ID : {applicantId}
+      <div>
+        <form>
+          <input
+            type="text"
+            name="name"
+            className="register_checkInfo_forms"
+            value={name}
+            onChange={onChange}
+            required
+          />
+          <input
+            type="text"
+            name="studentId"
+            className="register_checkInfo_forms"
+            value={studentId}
+            onChange={onChange}
+            required
+          />
+          <input
+            type="text"
+            name="ewhaianId"
+            className="register_checkInfo_forms"
+            value={ewhaianId}
+            onChange={onChange}
+            required
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default UpdatePage;
--- server/routes/update.js
+++ server/routes/update.js
@@ -1,47 +1,71 @@
 const express = require("express");
 const updateRoute = express.Router();
-
+const mongoose = require("mongoose");
 const { Applicant } = require("../models/Applicant");
 
+//id로 지원자 찾기
+updateRoute.post("/get", (req, res) => {
+  let id = req.body.id;
+  const ObjectId = mongoose.Types.ObjectId(id);
+  Applicant.findOne(
+    {
+      _id: ObjectId,
+    },
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    }
+  );
+});
 //수정하기
-updateRoute.put("/update", async (req, res) => { //async - await
+updateRoute.put("/update", async (req, res) => {
+  //async - await
 
-    const { name, studentId, ewhaianId } = req.body
+  const { name, studentId, ewhaianId } = req.body;
 
-    let applicantID = req.body.applicantID //ObjectID
+  let applicantID = req.body.applicantID; //ObjectID
 
-    let updatedData = {
-        name,
-        studentId,
-        ewhaianId
+  let updatedData = {
+    name,
+    studentId,
+    ewhaianId,
+  };
+  try {
+    await Applicant.findByIdAndUpdate(applicantID, { $set: updatedData }); //findByIDAndUpdate (조건:applicantID, 갱신: updatedData)
+    res.json({
+      messsage: "지원자 정보가 수정되었습니다",
+    });
+  } catch (error) {
+    if (!name || typeof name !== "string") {
+      return res.json({ status: "error", error: "성명을 입력하세요." });
     }
-    try {
-        await Applicant.findByIdAndUpdate(applicantID, { $set: updatedData }) //findByIDAndUpdate (조건:applicantID, 갱신: updatedData)
-        res.json({
-            messsage: "지원자 정보가 수정되었습니다"
-        })
-    } catch (error) {
-        if (!name || typeof name !== 'string') {
-            return res.json({ status: 'error', error: '성명을 입력하세요.' })
-        }
-        if (name.length > 10) {
-            return res.json({ status: 'error', error: '성명은 최대 10글자입니다.' })
-        }
-        if (!studentId || typeof studentId !== 'string') {
-            return res.json({ status: 'error', error: '학번을 입력하세요.' })
-        }
-        if (studentId.length !== 7) {
-            return res.json({ status: 'error', error: '학번은 7자리입니다.' })
-        }
-        if (!ewhaianId || typeof ewhaianId !== 'string') {
-            return res.json({ status: 'error', error: '이화이언 아이디를 입력하세요.' })
-        }
-        if (ewhaianId.length > 20) {
-            return res.json({ status: 'error', error: '이화이언 아이디는 최대 20글자입니다.' })
-        }
-        res.json({
-            message: "error occured"
-        })
+    if (name.length > 10) {
+      return res.json({ status: "error", error: "성명은 최대 10글자입니다." });
     }
+    if (!studentId || typeof studentId !== "string") {
+      return res.json({ status: "error", error: "학번을 입력하세요." });
+    }
+    if (studentId.length !== 7) {
+      return res.json({ status: "error", error: "학번은 7자리입니다." });
+    }
+    if (!ewhaianId || typeof ewhaianId !== "string") {
+      return res.json({
+        status: "error",
+        error: "이화이언 아이디를 입력하세요.",
+      });
+    }
+    if (ewhaianId.length > 20) {
+      return res.json({
+        status: "error",
+        error: "이화이언 아이디는 최대 20글자입니다.",
+      });
+    }
+    res.json({
+      message: "error occured",
+    });
+  }
 });
 module.exports = updateRoute;
