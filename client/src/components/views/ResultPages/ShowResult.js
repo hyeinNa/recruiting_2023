620090141eee45b8b59a4423dd56b70fd0c91a4b
rkdmsslasla <gaeun9477@gmail.com>
2023-03-07 03:56:47 +0900

    사전과제 추가

--- client/src/components/views/ResultPages/ShowResult.js
+++ client/src/components/views/ResultPages/ShowResult.js
@@ -32,12 +32,46 @@ function ShowResult() {
         console.log("An error occurred: ", error.response);
       });
   }, [applicantId]);
+
+  /*
+  useEffect(() => {
+    axios
+      .post("/api/var/load", {
+        key: 1234,
+      })
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.isInDB === "false") {
+          setIsMarketingPreAssignment("");
+          setIsDesignPreAssignment("");
+          setIsWebDevPreAssignment("");
+          setMarketingPreAssignment("");
+          setDesignPreAssignment("");
+          setWebDevPreAssignment("");
+        } else {
+          setIsMarketingPreAssignment(response.data.isMarketingPreAssignment);
+          setIsDesignPreAssignment(response.data.isDesignPreAssignment);
+          setIsWebDevPreAssignment(response.data.isWebDevPreAssignment);
+          setMarketingPreAssignment(response.data.marketingPreAssignment);
+          setDesignPreAssignment(response.data.master.designPreAssignment);
+          setWebDevPreAssignment(response.data.master.webDevPreAssignment);
+        }
+      });
+  }, []);
+
+  */
+
   /*
 default : (처음 지원할 때, 기본적으로 이것으로 설정)
 1stPass : 서류 합격
 finalPass : 최종 합격
 fail : 불합격
 */
+
+  //(1차합격)대면면접, 사전과제 O
+  //(1차합격)대면면접, 사전과제 x
+  //(1차합격)비대면면접, 사전과제 o
+  //(1차합격)비대면면접, 사전과제 x
   if (inputs.pass === "1stPass") {
     console.log(inputs.pass);
     return (
